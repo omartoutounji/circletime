18e9ab5eba8850a7ad9ec99cbfc7291c6c52bec5
Omar Toutounji <omartoutounji@icloud.com>
2024-01-09 11:16:07 -0500

    Connecting to Render backend

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -18,7 +18,7 @@ export default function Create() {
    e.preventDefault();
     // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
-    await fetch("http://localhost:1000/record/add", {
+    await fetch("https://circletime.onrender.com:1000/record/add", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -22,7 +22,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`http://localhost:1000/record/`);
+     const response = await fetch(`https://circletime.onrender.com:1000/record/`);
       if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
        window.alert(message);
@@ -36,7 +36,7 @@ export default function RecordList() {
  }, [records.length]);
   // This method will delete a record
  async function deleteRecord(id) {
-   await fetch(`http://localhost:1000/${id}`, {
+   await fetch(`https://circletime.onrender.com:1000/${id}`, {
      method: "DELETE"
    });
     const newRecords = records.filter((el) => el._id !== id);
