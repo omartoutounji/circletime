3383b1924b4ff808902376c279483718e170ebb7
Omar Toutounji <omartoutounji@icloud.com>
2024-01-09 13:59:31 -0500

    added geo abilities

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { getDistance } from 'geolib';
 import { Link } from "react-router-dom";
 const Record = (props) => (
  <tr>
    <td>{props.record.name}</td>
-   <td>{props.record.addr}</td>
+   <td>{props.distance/1000} kms away</td>
+   <td><Link to={"https://www.google.com/maps/search/?api=1&query=" + props.record.lat + "," + props.record.lng}>{props.record.addr}</Link></td>
    <td>{props.record.url}</td>
    <td>{props.record.tel}</td>
    <td>
@@ -20,10 +22,11 @@ const Record = (props) => (
 );
 export default function RecordList() {
  const [records, setRecords] = useState([]);
+ const [position, setPosition] = useState({ latitude: null, longitude: null });
   // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`https://circletime.onrender.com/record/`);
+     const response = await fetch(`http://localhost:1000/record/`);
       if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
        window.alert(message);
@@ -33,22 +36,29 @@ export default function RecordList() {
      setRecords(records);
    }
     getRecords();
+    navigator.geolocation.getCurrentPosition(function (position) {
+        setPosition({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        });
+    });
     return;
  }, [records.length]);
   // This method will delete a record
  async function deleteRecord(id) {
-   await fetch(`https://circletime.onrender.com/${id}`, {
+   await fetch(`http://localhost:1000/${id}`, {
      method: "DELETE"
    });
     const newRecords = records.filter((el) => el._id !== id);
    setRecords(newRecords);
  }
-  // This method will map out the records on the table
+  // This method will sort the records by distance and map out the records on the table
  function recordList() {
-   return records.map((record) => {
+   return records.sort((a, b) => getDistance(position, {latitude: a.lat, longitude: a.lng}) > getDistance(position, {latitude: b.lat, longitude: b.lng}) ? 1 : -1).map((record) => {
      return (
        <Record
          record={record}
+         distance={getDistance(position, {latitude: record.lat, longitude: record.lng})}
          deleteRecord={() => deleteRecord(record._id)}
          key={record._id}
        />
@@ -58,11 +68,12 @@ export default function RecordList() {
   // This following section will display the table with the records of individuals.
  return (
    <div>
-     <h3>Record List</h3>
+     <h3>EarlyONs near you</h3>
      <table className="table table-striped" style={{ marginTop: 20 }}>
        <thead>
          <tr>
            <th>Name</th>
+           <th>Distance</th>
            <th>Address</th>
            <th>Website</th>
            <th>Number</th>
