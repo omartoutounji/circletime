9fd4610fdc133c199ac156ac5db0d3cd775766b2
Omar Toutounji <omartoutounji@icloud.com>
2024-01-10 15:24:56 -0500

    Added styling

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from "react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import Grid from '@mui/material/Unstable_Grid2';
+import CallIcon from '@mui/icons-material/Call';
+import Typography from '@mui/material/Typography';
+import LinkIcon from '@mui/icons-material/Link';
 import { getDistance, getBoundsOfDistance } from 'geolib';
-import { Link } from "react-router-dom";
 const Record = (props) => (
- <tr>
-   <td>{props.record.name}</td>
-   <td>{props.distance/1000} kms away</td>
-   <td><Link to={"https://www.google.com/maps/search/?api=1&query=" + props.record.lat + "," + props.record.lng}>{props.record.addr}</Link></td>
-   <td>{props.record.url}</td>
-   <td>{props.record.tel}</td>
-   {/* <td>
-     <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
-     <button className="btn btn-link"
-       onClick={() => {
-         props.deleteRecord(props.record._id);
-       }}
-     >
-       Delete
-     </button>
-   </td> */}
- </tr>
+  <Card sx={{ maxWidth: 345}}>
+  <CardContent>
+    <Typography variant="h5" component="div">
+      {props.record.name}
+    </Typography>
+    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+    {props.distance/1000} kms away from you
+    </Typography>
+    <Typography variant="body2">
+    <Button href={props.record.url} variant="outlined" startIcon={<LinkIcon />}>
+  Website
+</Button>
+      <br />
+      <Button href={"tel:" + props.record.tel} variant="outlined" startIcon={<CallIcon />}>
+  Call
+</Button>
+    </Typography>
+  </CardContent>
+  <CardActions>
+  <Button href={"https://www.google.com/maps/search/?api=1&query=" + props.record.lat + "," + props.record.lng} variant="contained" endIcon={<DirectionsIcon />}>
+  Directions
+</Button>
+  </CardActions>
+  </Card>
+//  <tr>
+//    <td>{props.record.name}</td>
+//    <td>{props.distance/1000} kms away</td>
+//    <td><Link to={"https://www.google.com/maps/search/?api=1&query=" + props.record.lat + "," + props.record.lng}>{props.record.addr}</Link></td>
+//    <td>{props.record.url}</td>
+//    <td>{props.record.tel}</td>
+//    {/* <td>
+//      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
+//      <button className="btn btn-link"
+//        onClick={() => {
+//          props.deleteRecord(props.record._id);
+//        }}
+//      >
+//        Delete
+//      </button>
+//    </td> */}
+//  </tr>
 );
 export default function RecordList() {
  const [records, setRecords] = useState([]);
@@ -64,12 +97,19 @@ export default function RecordList() {
  function recordList() {
    return records.sort((a, b) => getDistance(position, {latitude: a.lat, longitude: a.lng}) > getDistance(position, {latitude: b.lat, longitude: b.lng}) ? 1 : -1).map((record) => {
      return (
-       <Record
+      <Grid xs={2} sm={4} md={4}>
+      <Box
+      component="section"
+      sx={{mx: '2px', transform: 'scale(0.8)'}}
+    >
+      <Record
          record={record}
          distance={getDistance(position, {latitude: record.lat, longitude: record.lng})}
          deleteRecord={() => deleteRecord(record._id)}
          key={record._id}
        />
+    </Box>
+    </Grid>
      );
    });
  }
@@ -77,18 +117,9 @@ export default function RecordList() {
  return (
    <div>
      <h3>EarlyONs near you</h3>
-     <table className="table table-striped" style={{ marginTop: 20 }}>
-       <thead>
-         <tr>
-           <th>Name</th>
-           <th>Distance</th>
-           <th>Address</th>
-           <th>Website</th>
-           <th>Number</th>
-         </tr>
-       </thead>
-       <tbody>{recordList()}</tbody>
-     </table>
+     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      {recordList()}
+      </Grid>
    </div>
  );
 }
